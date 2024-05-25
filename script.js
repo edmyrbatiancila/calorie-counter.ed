6b6c561bed9bff44a18a6114a02bc797109d7fd4
Edmyr <127021800+edmyrbatiancila@users.noreply.github.com>
2024-05-25 21:46:27 +0800

    Updated code in script.js

--- script.js
+++ script.js
@@ -21,8 +21,21 @@ function addEntry() {
     const targetInputContainer = document.querySelector(
         `#${entryDropdown.value} .input-container`
     );
-    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
     const HTMLString = `
         <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+        <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
+        <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+        <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories">
     `;
+    targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
+
+function getCaloriesFromInputs(list) {
+    let calories = 0;
+    for(const item of list) {
+        const currVal = item.value;
+    }
+}
+
+addEntryButton.addEventListener("click", addEntry);
