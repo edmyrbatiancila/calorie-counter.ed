e50da0bfbbe00e6a7570f306ef18818feefa0701
Edmyr <127021800+edmyrbatiancila@users.noreply.github.com>
2024-05-25 23:50:20 +0800

    updated code in script.js

--- script.js
+++ script.js
@@ -78,7 +78,31 @@ function calculateCalories(e) {
     const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
     const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
     const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
-    const HTMLString = output.innerHTML(``);
+    output.innerHTML = `
+        <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(
+        remainingCalories
+    )} Calorie ${surplusOrDeficit}</span>
+        <hr>
+        <p>${budgetCalories} Calories Budgeted</p>
+        <p>${consumedCalories} Calories Consumed</p>
+        <p>${exerciseCalories} Calories Burned</p>
+    `;
+
+    output.classList.remove('hide');
+}
+
+// below function is for Clearing the inputted form:
+function clearForm(){
+    const inputContainers = Array.from(document.querySelectorAll('.input-container'));
+
+    for(const container of inputContainers) {
+        container.innerHTML = "";
+    }
+    
+    budgetNumberInput.value = "";
 }
 
 addEntryButton.addEventListener("click", addEntry);
+
+// below is for Calculate Remaining Calories button logic:
+calorieCounter.addEventListener("submit", calculateCalories);
